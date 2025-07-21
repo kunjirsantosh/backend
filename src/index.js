7dd80bd3c3b8eb8b14e3ee3bdbf631ed46100a15
Santosh <kunjir.santosh@gmail.com>
2025-07-21 21:24:52 +0530

    mongoog connect
    mongoog connect

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./env"
+});
+
+
+connectDB();
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+//     ( async  () => {
+//         try{
+//             await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         } catch (error) {
+//             console.error(error)
+//         }
+// })();
+
+
